b1cf7e0ea2c9e31e3b8aceac86961f4026804a09
andreevadp <destroyapoison@yandex.ru>
2022-10-14 19:12:39 +0300

    TheSquareOfNumber added

---
+++ TheSquareOfNumber/Program.cs
@@ -0,0 +1,8 @@
+﻿//Простейший вариант:
+//int numberA = 4;
+//Console.WriteLine(numberA * numberA);
+
+Console.Write ("Введите число для вывода его в квадрат ");
+double A = Convert.ToDouble(Console.ReadLine());
+double number = A;
+Console.WriteLine(Math.Pow(number, 2)); //Console.WriteLine(numer * numer);
