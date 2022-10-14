c6e84525e610b737eb9c09b64fcc5a7f97d17344
andreevadp <destroyapoison@yandex.ru>
2022-10-14 19:46:54 +0300

    The Larger and the Smaller Number added

--- TheLargerAndTheSmallerNumber/Program.cs
+++ TheLargerAndTheSmallerNumber/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 5;
+int b = 7;
+int max;
+int min;
+if (a > b)
+{ 
+    max = a;
+    min = b;
+}
+else
+{
+    max = b;
+    min = a;
+}
+Console.WriteLine("Max number "+max);
+Console.WriteLine("Min number "+min);
